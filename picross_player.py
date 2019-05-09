b702778ef8732a8af2d7012329782d67a786bdbe
Alex <Alex@DESKTOP-KN7N240>
2019-05-09 15:54:12 -0600

    made printBoard print the board properly

--- picross_player.py
+++ picross_player.py
@@ -2,27 +2,27 @@ class Board:
     def __init__(self, boardSizeX, boardSizeY):
         self.boardSizeX = boardSizeX
         self.boardSizeY = boardSizeY
-        #self.numberArray = numberArray
-        self.boardArray[self.boardSizeX][self.boardSizeY]
-        for y in range(self.boardSizeY):
-            for x in range(self.boardSizeX):
-                self.boardArray[x][y] = 0
+        self.boardArray = [[0] * self.boardSizeY] * self.boardSizeX
+
 
 
     def printBoard(self):
-            for y in range(self.boardSizeY):
-                print("-")
-                for x in range(self.boardSizeX):
-                    print("|")
-                    if self.boardArray[x][y] == 0:
-                        print(" ")
-                    elif self.boardArray[x][y] == 1:
-                        print("X")
-                    else :
-                        print("#")
-                    print("|")
 
-                print("-")
+        print(self.boardArray)
+        for y in range(self.boardSizeY):
+                for x in range(self.boardSizeX):
+                    print("|", end="")
+                    if self.boardArray[x][y] == 0:
+                        print(" ", end="")
+                    elif self.boardArray[x][y] == 1:
+                        print("X", end="")
+                    else :
+                        print("#", end="")
+                print("|")
+                
+                for i in range((self.boardSizeX * 2) + 1):
+                    print("-", end="")
+                print("")
 
 b = Board(10,10)
 b.printBoard()
