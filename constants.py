a3da93a7a10d183c373abb0738464f66c54e2fd8
Alex <Alex@DESKTOP-KN7N240>
2019-05-10 09:11:16 -0600

    Fixed the boardArray so it no longer sets all the columns when a value is changed

---
+++ constants.py
@@ -0,0 +1,3 @@
+empty = 0
+filled = 1
+marked = 2
--- picross_player.py
+++ picross_player.py
@@ -1,20 +1,26 @@
+
 class Board:
     def __init__(self, boardSizeX, boardSizeY):
+        self.empty = 0
+        self.filled = 1
+        self.marked = 2
         self.boardSizeX = boardSizeX
         self.boardSizeY = boardSizeY
-        self.boardArray = [[0] * self.boardSizeY] * self.boardSizeX
+        self.boardArray = [[0 for i in range(self.boardSizeX)] for j in range(self.boardSizeY)]
+
 
 
 
     def printBoard(self):
-
-        print(self.boardArray)
+        for i in range((self.boardSizeX * 2) + 1):
+            print("-", end="")
+        print("")
         for y in range(self.boardSizeY):
                 for x in range(self.boardSizeX):
                     print("|", end="")
-                    if self.boardArray[x][y] == 0:
+                    if self.boardArray[x][y] == self.empty:
                         print(" ", end="")
-                    elif self.boardArray[x][y] == 1:
+                    elif self.boardArray[x][y] == self.filled:
                         print("X", end="")
                     else :
                         print("#", end="")
@@ -23,6 +29,17 @@ class Board:
                 for i in range((self.boardSizeX * 2) + 1):
                     print("-", end="")
                 print("")
+    
+                
+    def fillIn(self,x,y):
+        self.boardArray[x][y] = self.filled
+
+
+
 
 b = Board(10,10)
+b.printBoard()
+
+print("\n\n")
+b.fillIn(2,0)
 b.printBoard()
